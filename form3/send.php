321a8f7d0b5df34cf7127f1f2c8419404c70dfa7
Mihail Ziljajev <jktrut@gmail.com>
2023-11-01 13:10:15 +0200

    Second commit

--- form3/send.php
+++ form3/send.php
@@ -1,32 +1,30 @@
-<html>
-<head>
-<title>Форма заявки с сайта</title>
-</head>
-<body>
-<?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['fio']) and !isset($_POST['email'])){
- ?> <form action="send.php" method="post">
-<input type="text" name="fio" placeholder="Укажите ФИО" required>
-<input type="text" name="email" placeholder="Укажите e-mail" required>
-<input type="submit" value="Отправить">
-</form> <?php
+&lt;?php
+require 'PHPMailerAutoload.php';
+ 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+ 
+$mail = new PHPMailer;
+$mail-&gt;isSMTP();
+$mail-&gt;Host = 'smtp.example.com';
+$mail-&gt;SMTPAuth = true;
+$mail-&gt;Username = 'your_email@example.com';
+$mail-&gt;Password = 'your_email_password';
+$mail-&gt;SMTPSecure = 'tls';
+$mail-&gt;Port = 587;
+ 
+$mail-&gt;setFrom($email, $name);
+$mail-&gt;addAddress('your_email@example.com');
+$mail-&gt;isHTML(true);
+ 
+$mail-&gt;Subject = 'Сообщение с формы обратной связи';
+$mail-&gt;Body    = "От: $name &lt;br&gt; Email: $email &lt;br&gt; Сообщение: $message";
+ 
+if(!$mail-&gt;send()) {
+    echo 'Ошибка при отправке сообщения.';
+    echo 'Mailer Error: ' . $mail-&gt;ErrorInfo;
 } else {
- //показываем форму
- $fio = $_POST['fio'];
- $email = $_POST['email'];
- $fio = htmlspecialchars($fio);
- $email = htmlspecialchars($email);
- $fio = urldecode($fio);
- $email = urldecode($email);
- $fio = trim($fio);
- $email = trim($email);
- if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
- echo "Сообщение успешно отправлено";
- } else {
- echo "При отправке сообщения возникли ошибки";
- }
+    echo 'Сообщение успешно отправлено!';
 }
-?>
-</body>
-</html>
+?&gt;
